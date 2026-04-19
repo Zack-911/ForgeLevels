ffee0de478cf08b3a4b81656dbe491241235fc0d
Zack <muhammadbinasim471@gmail.com>
2026-04-19 17:53:38 +0500

    fix: swap logic and naming between $levelEventNewLevel and $levelEventOldLevel functions

--- src/functions/events/levelEventOldLevel.ts
+++ src/functions/events/levelEventOldLevel.ts
@@ -2,14 +2,14 @@ import { ArgType, NativeFunction } from "@tryforge/forgescript"
 import { ILevelEvents } from "../../structures/eventManager"
 
 export default new NativeFunction({
-    name: "$levelEventNewLevel",
+    name: "$levelEventOldLevel",
     version: "1.0.0",
-    description: "Inside a levelUp event command, returns the new level.",
+    description: "Inside a levelUp event command, returns the old level.",
     unwrap: true,
     output: ArgType.Number,
     args: [],
     async execute(ctx) {
         // @ts-ignore
-        return this.success((ctx.runtime.extras as ILevelEvents["levelUp"][0]).newLevel)
+        return this.success((ctx.runtime.extras as ILevelEvents["levelUp"][0]).oldLevel)
     },
 })
